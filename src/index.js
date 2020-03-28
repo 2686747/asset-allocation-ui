8a0edea2f68a8d7c396a9feafced5c96b9470e46
Maksim Vakhnik <vmk80@mail.ru>
2020-03-28 16:45:59 +0300

    close #3 Add inferno framework
    I wanted to use fast framework (based on 3rd party benchmarks)
    I tried to use 'mikado' as fastest in the chart but it's hard to work
    with it without good documentation. Also it looks not very active (3
    Oct 2019). So i dediced to choose 'inferno' as:
    - i had experience
    - it looks like React
    - it's fast

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import 'purecss';
+import { render } from 'inferno';
+import { createElement } from 'inferno-create-element';
+
 console.log('loaded1');
+
+render(
+  createElement('div', { className: 'test' }, "I'm a child!"),
+  document.getElementById("app")
+);
